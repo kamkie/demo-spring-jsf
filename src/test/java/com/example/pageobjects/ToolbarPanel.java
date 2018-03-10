c8d291eecfd6bb33efe3496093903e4c3c2645ed
Kamil Kiewisz <kamkie@outlook.com>
2018-03-10 22:58:33 +0100

    booRun include source resources, fixed tests

--- src/test/java/com/example/pageobjects/ToolbarPanel.java
+++ src/test/java/com/example/pageobjects/ToolbarPanel.java
@@ -1,8 +1,10 @@
 package com.example.pageobjects;
 
+import com.example.util.JsfUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Locale;
 
@@ -15,7 +17,9 @@ public class ToolbarPanel {
     }
 
     public void changeLanguage(Locale locale) {
-        Select languageSelectBox = new Select(webDriver.findElement(By.id("toolbarForm:localeForm:language-select-box")));
+        Select languageSelectBox = new Select(webDriver.findElement(By.id("localeForm:language-select-box")));
         languageSelectBox.selectByValue(locale.getLanguage());
+        WebDriverWait wait = new WebDriverWait(webDriver, 30);
+        wait.until(JsfUtil.waitForJQueryAndPrimeFaces());
     }
 }
