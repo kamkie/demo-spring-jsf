e448f4a80aead8bd2eeeda682a65cc65f083d7b8
Kamil Kiewisz <kamkie@outlook.com>
2019-06-23 18:39:56 +0200

    debug failing selenium in travis

--- configure.sh
+++ configure.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-HOST_FOR_SELENIUM="$(/sbin/ip route|awk '/default/ { print $3 }')"
+
+ip addr show docker0
+#HOST_FOR_SELENIUM="$(/sbin/ip route|awk '/default/ { print $3 }')"
+HOST_FOR_SELENIUM="172.17.0.1"
 export HOST_FOR_SELENIUM
 echo "$HOST_FOR_SELENIUM"
