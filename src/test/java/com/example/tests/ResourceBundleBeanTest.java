4281903f15188f3a1e0aa409f780bbbfbf9f2bc1
Kamil Kiewisz <kamkie@outlook.com>
2019-06-23 20:07:33 +0200

    ignore spotbug in ResourceBundleBeanTest.java

--- src/test/java/com/example/tests/ResourceBundleBeanTest.java
+++ src/test/java/com/example/tests/ResourceBundleBeanTest.java
@@ -10,8 +10,9 @@ import java.util.HashMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ResourceBundleBeanTest {
-    @SuppressFBWarnings("NP_NONNULL_PARAM_VIOLATION")
+
     @Test
+    @SuppressFBWarnings("NP_NONNULL_PARAM_VIOLATION")
     void testEquals() {
         ResourceBundleBean resourceBundleBean = new ResourceBundleBean(null, null);
 
@@ -19,14 +20,15 @@ class ResourceBundleBeanTest {
     }
 
     @Test
+    @SuppressFBWarnings("NP_NONNULL_PARAM_VIOLATION")
     void testEntrySet() {
         ResourceBundleBean resourceBundleBean = new ResourceBundleBean(null, null);
 
         assertThat(resourceBundleBean.entrySet()).hasSize(0);
     }
 
-    @SuppressFBWarnings("NP_NONNULL_PARAM_VIOLATION")
     @Test
+    @SuppressFBWarnings("NP_NONNULL_PARAM_VIOLATION")
     void testNotEquals() {
         assertThat(new ResourceBundleBean(new LocaleModel(null, null), null))
                 .isNotEqualTo(new ResourceBundleBean(null, null))
