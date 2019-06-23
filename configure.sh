760c6d5da0face3550737753e27af21a72e3b41c
Kamil Kiewisz <kamkie@outlook.com>
2019-06-23 18:58:27 +0200

    remove debug logs

--- configure.sh
+++ configure.sh
@@ -5,4 +5,4 @@ ip addr show docker0
 #HOST_FOR_SELENIUM="$(/sbin/ip route|awk '/default/ { print $3 }')"
 HOST_FOR_SELENIUM="172.17.0.1"
 export HOST_FOR_SELENIUM
-echo "$HOST_FOR_SELENIUM"
+echo "docker host ip: $HOST_FOR_SELENIUM"
