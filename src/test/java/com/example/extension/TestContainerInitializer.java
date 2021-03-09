2ad211d72138d4dd5482d1c63fb85a95277ac9c9
Kamil Kiewisz <kamkie@outlook.com>
2021-03-09 20:52:16 +0100

    use test containers for selenium

--- src/test/java/com/example/extension/TestContainerInitializer.java
+++ src/test/java/com/example/extension/TestContainerInitializer.java
@@ -1,7 +1,6 @@
 package com.example.extension;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,17 +9,19 @@ import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.springframework.boot.test.util.TestPropertyValues.Type.SYSTEM_ENVIRONMENT;
 
+@Slf4j
 public class TestContainerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestContainerInitializer.class);
+
+    private static final PostgreSQLContainer POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:12");
 
     @Override
     public void initialize(@NonNull ConfigurableApplicationContext configurableApplicationContext) {
-        PostgreSQLContainer postgreSQLContainer = DockerExtension.getPostgres();
-        LOGGER.info("------- initializing postgres config for test containers with port {} -------", postgreSQLContainer.getPortBindings());
+        POSTGRE_SQL_CONTAINER.start();
+        log.info("------- initializing postgres config for test containers with port {} -------", POSTGRE_SQL_CONTAINER.getPortBindings());
         TestPropertyValues.of(
-                "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                "spring.datasource.password=" + postgreSQLContainer.getPassword())
+                "spring.datasource.url=" + POSTGRE_SQL_CONTAINER.getJdbcUrl(),
+                "spring.datasource.username=" + POSTGRE_SQL_CONTAINER.getUsername(),
+                "spring.datasource.password=" + POSTGRE_SQL_CONTAINER.getPassword())
                 .applyTo(configurableApplicationContext.getEnvironment(), SYSTEM_ENVIRONMENT, "testPropertySource");
     }
 }
