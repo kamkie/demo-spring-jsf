6b9c117514c6cd32d1dd5ed7712c7100108adbd1
Kamil Kiewisz <kamkie@outlook.com>
2022-12-06 20:42:13 +0100

    space build

--- .space.kts
+++ .space.kts
@@ -4,6 +4,15 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
+job("build") {
+    container(image = "gradle:7.5-jdk17", displayName = "gradle") {
+        kotlinScript { api ->
+            api.gradlew("build")
+
+            // run publish task for release branches
+            if (api.gitBranch().contains("release")) {
+                api.gradlew("publish")
+            }
+        }
+    }
 }
