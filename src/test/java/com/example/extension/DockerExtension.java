b7cfbbc57fc8450195fd790534ec19c25c0c9352
Kamil Kiewisz <kamkie@outlook.com>
2022-12-29 04:55:16 +0100

    use testcontainers-selenium

--- src/test/java/com/example/extension/DockerExtension.java
+++ src/test/java/com/example/extension/DockerExtension.java
@@ -1,20 +1,28 @@
 package com.example.extension;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.Extension;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-@SuppressWarnings("PMD.UseUtilityClass")
+@Slf4j
+@SuppressWarnings({
+        "PMD.UseUtilityClass",
+        "PMD.DoNotUseThreads"
+})
 public class DockerExtension implements Extension {
 
     private static final PostgreSQLContainer POSTGRES_SQL_CONTAINER = new PostgreSQLContainer("postgres:14");
 
-    @SuppressWarnings("PMD.DoNotUseThreads")
-    public DockerExtension() {
-        Runtime.getRuntime().addShutdownHook(new Thread(POSTGRES_SQL_CONTAINER::stop));
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("stopping postgres container");
+            POSTGRES_SQL_CONTAINER.stop();
+        }));
     }
 
     public static PostgreSQLContainer getPostgres() {
         if (!POSTGRES_SQL_CONTAINER.isRunning()) {
+            log.info("starting postgres container");
             POSTGRES_SQL_CONTAINER.start();
         }
         return POSTGRES_SQL_CONTAINER;
